69fa9c47a31cd3f773b7d03de85e0d70eceb6a5a
KayKay-H <n01587541@humber.ca>
2025-02-02 21:55:01 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,55 @@
+var studentName = "Kayle Krystal Hunyinbo";
+let studentAge = 23;
+let isEnrolled = true;
+
+console.log(studentName);
+console.log(studentAge);
+console.log(isEnrolled);
+
+let businessName = "Krystevents";
+let businessType = "Event Planning";
+console.log(businessName);
+console.log(businessType);
+
+let yearsOfExperience = 1;
+console.log(yearsOfExperience);
+
+let certified = false;
+let isRegistered = true;
+console.log(certified);
+console.log(isRegistered);
+
+let services = ['Wedding Planning', 'Birthday Planning', 'Corporate Event Planning'];
+console.log(services);
+
+let person = {
+    name: "Kayle",
+    age: 23,
+    isEnrolled: true
+};
+console.log(person.name);
+console.log(person.age);
+
+console.log(24+20);
+console.log(11-8);
+console.log(8*5);
+console.log(28/2);
+
+let x = 250;
+let y = 76;
+
+console.log(x<y);
+console.log(x>y);
+console.log(x==y);
+console.log(x!=y);
+
+let a = true;
+let b = false;
+console.log(a && b);
+console.log(a || b);
+console.log(!a);
+
+
+
+
+
